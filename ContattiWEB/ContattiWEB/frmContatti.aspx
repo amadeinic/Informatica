0dc93519b911197201fbdb3ce7c10cdb037b28ba
Nicola Amadei <nicola.amadei@gmail.com>
2016-02-24 13:14:29 +0100

    Added ContattiWEB
    Progetto per la visualizzazione semplice di alcuni contatti e chiamate

---
+++ ContattiWEB/ContattiWEB/frmContatti.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmContatti.aspx.cs" Inherits="ContattiWEB.frmContatti" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ContattiWEB - Nicola Amadei 2016</title>
+    <style type="text/css">
+        .titolo {
+            font-weight: normal;
+            color: #1E90FF;
+            letter-spacing: 1pt;
+            word-spacing: 2pt;
+            font-size: 41px;
+            text-align: center;
+            font-family: trebuchet MS, sans-serif;
+            line-height: 1;
+        }
+        .auto-style1 {
+            width: 360px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <h1 class="titolo">ContattiWEB - Nicola Amadei 2016</h1>
+        <center>
+            <table border="1">
+                <tr>
+                    <td>
+                        <asp:Label ID="lblQuery" runat="server" Text="Query a manona:"></asp:Label>
+                    </td>
+                    <td class="auto-style1">
+                        <asp:TextBox ID="txtQuery" runat="server" Height="122px" TextMode="MultiLine" Width="354px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Scelta:"></asp:Label>
+                    </td>
+                    <td class="auto-style1">                                                  
+                    <asp:DropDownList ID="ddlScelta" runat="server">
+                            <asp:ListItem>Tutte le chiamate</asp:ListItem>
+                            <asp:ListItem>Ultime 15 chiamate</asp:ListItem>
+                            <asp:ListItem>Ultime 5 a persone simpatiche</asp:ListItem>
+                            <asp:ListItem>Ultime 5 a persone antipatiche</asp:ListItem>
+                    </asp:DropDownList>                    
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="2">
+                        <asp:Button ID="btnEsegui" runat="server" Text="Esegui" OnClick="btnEsegui_Click" Width="150"/>  
+                    </td>
+                </tr>             
+            </table>                      
+            <br />
+            <br />
+            <asp:Image ID="imgStato" runat="server" ImageUrl="~/Images/wait.png" />
+            <br />
+            <asp:GridView ID="gvTabella" runat="server" Height="169px" Width="320px">
+            </asp:GridView>
+             </center>  
+        </div>
+    </form>
+</body>
+</html>
---
+++ ContattiWEB/ContattiWEB/frmContatti.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ContattiWEB
+{
+    public partial class frmContatti : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEsegui_Click(object sender, EventArgs e)
+        {
+            string query = ""; 
+            if (txtQuery.Text=="")
+            {
+                switch(ddlScelta.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            query = "SELECT DataOra, Nome, Cognome, Simpatia, Motivo "+
+                                    "FROM Contatti INNER JOIN Chiamate ON Contatti.IdContatto = Chiamate.IdContatto "+
+                                    "ORDER BY DataOra";
+                            break;
+                        }
+                    case 1:
+                        {
+                            query = "SELECT TOP 15 DataOra, Nome, Cognome, Simpatia, Motivo " +
+                                    "FROM Contatti INNER JOIN Chiamate ON Contatti.IdContatto = Chiamate.IdContatto " +
+                                    "ORDER BY DataOra";
+                            break;
+                        }
+                    case 2:
+                        {
+                            query = "SELECT TOP 5 DataOra, Nome, Cognome, Simpatia, Motivo " +
+                                    "FROM Contatti INNER JOIN Chiamate ON Contatti.IdContatto = Chiamate.IdContatto " +
+                                    "WHERE Simpatia>=3 "+
+                                    "ORDER BY DataOra";
+                            break;
+                        }
+                    case 3:
+                        {
+                            query = "SELECT TOP 5 DataOra, Nome, Cognome, Simpatia, Motivo " +
+                                    "FROM Contatti INNER JOIN Chiamate ON Contatti.IdContatto = Chiamate.IdContatto " +
+                                    "WHERE Simpatia<3 " +
+                                    "ORDER BY DataOra";
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                query = txtQuery.Text;
+            }
+            
+            
+            try
+            {
+                //Connetto al DB !!!!!!PERCORSO ASSOLUTO!!!!!!      
+                OleDbConnection cnn = new OleDbConnection();
+                cnn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+                    @"Data source=" + Server.MapPath("~") +
+                    @"\Appdata\contatti.mdb";
+                cnn.Open();
+                //Creiamo una DataTable per contenere i nostri dati.
+                DataTable dt = new DataTable();
+                //Creiamo il DataAdapter che sfrutterà la nostra connessione per eseguire la query e prelevare i dati.
+                OleDbDataAdapter da = new OleDbDataAdapter(query, cnn);
+                //Riempiamo la DataTable            
+                da.Fill(dt);
+                //Aggancio il datatable alla gridview
+                gvTabella.DataSource = dt;
+                gvTabella.DataBind();
+                imgStato.ImageUrl = @".\Images\ok.png";
+            }
+            catch (Exception exc)
+            {
+
+                //Visualizzo un alert
+                string script = "alert("+exc.Message+");";
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                                      "ServerControlScript", script, true);
+                //Segnalo all'utente che la query non è andata a buon fine pulendo la tabella e impostando l'immagine rossa
+                imgStato.ImageUrl = @".\Images\wait.png";
+                gvTabella.DataSource = null;
+                gvTabella.DataBind();
+            }
+        }
+    }
+}
